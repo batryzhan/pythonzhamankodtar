9ee2a0729e32bdf3253c7b4cd1253de3c22030f3
Batyrzhan <amanb2k7@gmail.com>
2025-07-13 17:05:19 +0500

    Create smsbomber.py

---
+++ smsbomber.py
@@ -0,0 +1,14 @@
+import pyautogui 
+import time
+
+a = int(input("how many times?: "))
+
+b = input("message: ")
+
+time.sleep(3)
+
+for i in range(a):
+    pyautogui.write(b)
+    pyautogui.press('enter')
+
+print("done")
